932c263cf8167924a18dc5adf65c0b67d3e15692
lakshya1goel <118365845+lakshya1goel@users.noreply.github.com>
2023-08-23 22:46:34 +0530

    create recent chats page

--- lib/Models/ChatRoomModel.dart
+++ lib/Models/ChatRoomModel.dart
@@ -1,13 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 class ChatRoomModel{
   String? chatRoomId;
   Map<String, dynamic>? participants;
   String? lastMsg;
-  ChatRoomModel({required this.chatRoomId, required this.participants, required this.lastMsg});
+  Timestamp? time;
+  ChatRoomModel({required this.chatRoomId, required this.participants, required this.lastMsg, required this.time});
 
   ChatRoomModel.fromMap(Map<String, dynamic> map){
     chatRoomId=map["chatRoomId"];
     participants=map["participants"];
     lastMsg=map["lastMsg"];
+    time=map["time"];
   }
 
   Map<String, dynamic> toMap(){
@@ -15,6 +18,7 @@ class ChatRoomModel{
       "chatRoomId":chatRoomId,
       "participants":participants,
       "lastMsg":lastMsg,
+      "time":time,
     };
   }
 }
--- lib/Services/Chats/contacts.dart
+++
@@ -1,8 +0,0 @@
-class contacts{
-  String image;
-  String name;
-  String msg;
-  Duration time;
-  bool isactive;
-  contacts({required this.image, required this.name, required this.msg, required this.time, this.isactive=false});
-}
